03a8fd511b72292a9d490c9ae01a442ee7cc9cc2
Axley Herrera <axley01herrera@gmail.com>
2023-09-30 15:57:31 +0100

    shoptpv

--- db/shoptpv.sql
+++ db/shoptpv.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 26-08-2023 a las 19:21:47
--- Versión del servidor: 5.7.36
--- Versión de PHP: 7.4.26
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `shoptpv`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `shop_basket`
---
-
 DROP TABLE IF EXISTS `shop_basket`;
 CREATE TABLE IF NOT EXISTS `shop_basket` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -35,13 +6,7 @@ CREATE TABLE IF NOT EXISTS `shop_basket` (
   `date` date DEFAULT NULL,
   `payType` int(1) DEFAULT NULL COMMENT '1 = Cash\r\n2 = Card',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `shop_basket_product`
---
+) 
 
 DROP TABLE IF EXISTS `shop_basket_product`;
 CREATE TABLE IF NOT EXISTS `shop_basket_product` (
@@ -50,13 +15,7 @@ CREATE TABLE IF NOT EXISTS `shop_basket_product` (
   `fk_product` int(11) NOT NULL,
   `amount` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='join table to relate basket with products ';
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `shop_config`
---
+) 
 
 DROP TABLE IF EXISTS `shop_config`;
 CREATE TABLE IF NOT EXISTS `shop_config` (
@@ -74,13 +33,7 @@ CREATE TABLE IF NOT EXISTS `shop_config` (
   `password` varchar(999) COLLATE utf8_spanish_ci NOT NULL DEFAULT '$2y$10$AqHRm7ncB4yYCLYQv7yDvu5amAsD7RQ.GDHH/O84RgthbT1tiHIfu',
   `printer` varchar(999) COLLATE utf8_spanish_ci DEFAULT 'smb://AcerNitroDev/POS80 Printer',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `shop_product`
---
+) 
 
 DROP TABLE IF EXISTS `shop_product`;
 CREATE TABLE IF NOT EXISTS `shop_product` (
@@ -90,9 +43,4 @@ CREATE TABLE IF NOT EXISTS `shop_product` (
   `price` float NOT NULL,
   `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+)
